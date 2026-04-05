f396b3dda9551ad7d85b368903a494392824479e
vmgorelikov <vmgorelikov@edu.hse.ru>
2026-04-05 19:21:02 +0300

    add schemata for translation requests

---
+++ src/schemas/translation.py
@@ -0,0 +1,36 @@
+from sqlmodel import SQLModel
+from typing import Any, Dict, Literal, Optional
+from sqlmodel import Field, SQLModel
+from pydantic import RootModel
+
+class SourceText(SQLModel):
+    '''
+    Представление текста, получаемого от клиента.
+    '''
+    original: str
+
+class Synonyms(RootModel):
+    '''
+    Представление списка синонимов к словам и выражением в переводе.
+    '''
+    root: Dict[str, set[str]]
+
+class TranslationChunk(SQLModel):
+    '''
+    Представление чанка перевода и сопутствующих данных
+    для отправки клиенту.
+    '''
+    id: int
+    translated: str
+    index: int
+    properties: list[Synonyms]
+    is_final: bool
+
+class TranslationResult:
+    '''
+    Представление полного перевода.
+    '''
+    id: int
+    translated: str
+    properties: list[Synonyms]
+

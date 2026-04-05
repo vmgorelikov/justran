2bed6e3aea5f2d584b26a49f5efd7d5dfb734e84
vmgorelikov <vmgorelikov@edu.hse.ru>
2026-04-05 15:34:13 +0300

    add docstrings

--- src/schemas/auth.py
+++ src/schemas/auth.py
@@ -1,14 +1,42 @@
+'''
+Схемы данных, используемые при авторизации.
+
+:Authors: В. М. Гореликов <vmgorelikov@edu.hse.ru>
+'''
+
 from sqlmodel import SQLModel
 from typing import Literal, Optional
+from sqlmodel import Field, SQLModel
 
 class UserCredentials(SQLModel, table=False):
-    username: str
+    '''
+    Пара логин:пароль пользователя.
+
+    :var username: Логин до 32 символов из `[A-Za-z0-9_\-.]`.
+    :var password: Пароль в явном виде.
+    '''
+    username: str = Field(max_length=32, regex=r'[A-Za-z0-9_\-.]+')
     password: str
 
 class Token(SQLModel, table=False):
+    '''
+    Токен для доступа к API.
+    :var access_token: Токен.
+    :var token_type: Тип токена (всегда Bearer).
+    '''
     access_token: str
     token_type: Literal['bearer']
 
 class AuthError(SQLModel, table=False):
+    '''
+    Ошибка авторизации.
+    :var error: Код ошибки:
+    * `invalid_token` — токен осутствует, истёк, принадлежит
+    удалённому пользователю или просто неверный.
+    * `invalid_credentials` — логин и/или пароль, представленные
+    для получения токена, не подходят.
+
+    :var error_description: Не используется.
+    '''
     error: Literal['invalid_token'] | Literal['invalid_credentials']
     error_description: Optional[str]
